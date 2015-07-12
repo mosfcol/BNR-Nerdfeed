3f73b4457bee369bfd86e37c13bc14f71b3a3268
Shaofeng Mo <mosfcol@gmail.com>
2015-07-12 16:17:25 -0700

    init commit

---
+++ Nerdfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Shaofeng Mo on 7/12/15.
+//  Copyright (c) 2015 Seanmok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
